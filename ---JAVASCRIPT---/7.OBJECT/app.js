7159ac1447848e91d79a86b4ba1260fec428eb17
Mohammad Salman <mohdsalman5270@gmail.com>
2024-09-13 23:14:10 +0530

    Add files via upload

---
+++ ---JAVASCRIPT---/7.OBJECT/app.js
@@ -0,0 +1,81 @@
+// Object.
+
+const student ={
+
+   name:"salman",
+   age: 20,
+   city: "Delhi",
+};
+
+//Q1; Create a post using object literals for Thread/Twitter.
+
+const post={
+
+    username : "mohammad salman",
+    content : "#firstpost",
+    likes : 500,
+    repost : 10,
+    tags : ["@soban","@khalid"]
+};
+
+// How to get value in object literals..
+
+//   1-  post["username"]
+//   2-  post.username
+
+// Add & Update in object.
+
+
+const student1 = {
+
+    name: "shradha",
+    age : 25,
+    gender : "Male",
+    City : "Delhi"
+}
+
+
+// Object of objects.
+
+const  classInfo={
+
+        shardha:{
+            grade:"A+",
+            city : "delhi"
+        },
+
+        aman:{
+            grade:"B+",
+            city : "mumbai"  
+         },
+
+        salman:{
+            grade : "B",
+            city : "jaipur"
+        }
+};
+
+
+//Array of Objects.
+
+const studentinfo=[
+    {
+     name : "salman",
+     grade :"B+",
+     City : "mumbai"
+
+    },
+
+    {
+     name : "khan",
+     grade :"B",
+     city : "delhi"
+    },
+
+    {
+     name : "soban",
+     grade :"c+",
+     City : "noida"
+    },
+
+];
